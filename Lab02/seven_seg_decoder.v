eb13a7c49188b6b61720bc0ffbe9d896c8d91aa1
JPrime1 <james.prime.dev@gmail.com>
2026-02-01 20:09:33 -0500

    Finished 7 segment display decoder + comments

--- Lab02/seven_seg_decoder.v
+++ Lab02/seven_seg_decoder.v
@@ -1,9 +1,23 @@
 module seven_seg_decoder(
-	[3:0] in,
-	[7:0] seg);
-	
-	input in;
-	output seg;
-	
+	input [3:0] in,
+	output reg [7:0] seg);
+
+	always @(*) begin
+		case (in)
+			4'd0: seg[6:0] = 7'b1000000; // 0
+			4'd1: seg[6:0] = 7'b1111001; // 1
+			4'd2: seg[6:0] = 7'b0100100; // 2
+			4'd3: seg[6:0] = 7'b0110000; // 3
+			4'd4: seg[6:0] = 7'b0011001; // 4
+			4'd5: seg[6:0] = 7'b0010010; // 5
+			4'd6: seg[6:0] = 7'b0000010; // 6
+			4'd7: seg[6:0] = 7'b1111000; // 7
+			4'd8: seg[6:0] = 7'b0000000; // 8
+			4'd9: seg[6:0] = 7'b0010000; // 9
+   			default: seg[6:0] = 7'b111_1111; // default: all segments OFF
+		endcase
+
+		seg[7] = 1'b1; //dp OFF
+	end
 	
 endmodule
