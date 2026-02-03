43eacd36db747a8e93376f9852783fc4aef83858
JPrime1 <James.Prime.Dev@gmail.com>
2026-02-03 15:25:29 -0500

    Removed priority circuit

--- Lab02/Lab02.v
+++ Lab02/Lab02.v
@@ -2,20 +2,15 @@ module Lab02 (
     input [8:0] SW,
 	output [7:0] HEX0);
 
-    wire [8:0] pr_en; //priority encoded input
-    priority_encoder encode(.in(SW), .en(pr_en));
+    wire [3:0] val; //four bit value conversion for student number
 
-    wire [3:0] val; //four bit value conversion
-    assign val[0] = (~|pr_en) | pr_en[0] | pr_en[1] | pr_en[6] | pr_en[7];
-    assign val[1] = (~|pr_en) | pr_en[0] | pr_en[2] | pr_en[4] | pr_en[8];
-    assign val[2] = (~|pr_en) | pr_en[0] | pr_en[1] | pr_en[2] | pr_en[6];
-    assign val[3] = (~|pr_en) | pr_en[3];
+    assign val[0] = (~|SW) | SW[0] | SW[1] | SW[6] | SW[7];
+    assign val[1] = (~|SW) | SW[0] | SW[2] | SW[4] | SW[8];
+    assign val[2] = (~|SW) | SW[0] | SW[1] | SW[2] | SW[6];
+    assign val[3] = (~|SW) | SW[3];
 
-    //test instructions
-    // assign val = SW[3:0];
-
+    //Display value onto HEX0 led segmented display
     seven_seg_decoder_bool display(.in(val), .seg(HEX0));
-    //seven_seg_decoder(.in(val), .seg(HEX0));
 
     
 endmodule
