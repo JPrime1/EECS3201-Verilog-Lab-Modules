0e74f1203ae14478fc2a659c5825efe97d70d116
JPrime1 <James.Prime.Dev@gmail.com>
2026-02-03 10:04:02 -0500

    display test for 4 switches as numerical values

--- Lab02/Lab02.v
+++ Lab02/Lab02.v
@@ -6,10 +6,13 @@ module Lab02 (
     priority_encoder encode(.in(SW), .en(pr_en));
 
     wire [3:0] val; //four bit value conversion
-    assign val[0] = pr_en[0] | pr_en[1] | pr_en[6];
+    /*assign val[0] = pr_en[0] | pr_en[1] | pr_en[6];
     assign val[1] = pr_en[0] | pr_en[2] | pr_en[4] | pr_en[5] | pr_en[7];
     assign val[2] = pr_en[0] | pr_en[1] | pr_en[2] | pr_en[5];
-    assign val[3] = pr_en[3];
+    assign val[3] = pr_en[3];*/
+
+    //test instructions
+    assign [3:0] val = [3:0] SW;
 
     seven_seg_decoder_bool display(.in(val), .seg(HEX0));
     //seven_seg_decoder(.in(val), .seg(HEX0));
