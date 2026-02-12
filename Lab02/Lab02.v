40afc2285f79600015419fccf336bb13c76baf6c
JPrime1 <James.Prime.Dev@gmail.com>
2026-02-12 15:00:50 -0500

    added lab02 description

--- Lab02/Lab02.v
+++ Lab02/Lab02.v
@@ -1,3 +1,20 @@
+//============================================================
+// EECS 3201 – Lab 2
+// 7-Segment Display Driver
+//
+// Student Name: James Prime
+// Student Number: 215028657
+//
+// GitHub Repository:
+// https://github.com/JPrime1/EECS3201-Verilog-Lab-Modules.git
+//
+// Description:
+// This project implements a 7-segment display driver using
+// minimized Boolean functions. Switches SW8–SW0 select and
+// display the digits of the student number.
+//
+//============================================================
+
 module Lab02 (
     input [8:0] SW,
 	output [7:0] HEX0);
