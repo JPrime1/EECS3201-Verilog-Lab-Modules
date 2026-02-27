790c51f0ec8c1e8e615d3ad1a351df0884a1b1f8
JPrime1 <James.Prime.Dev@gmail.com>
2026-02-27 10:49:22 -0500

    changed syntax to reflect better modularity

--- Lab03/symbol_logic.v
+++ Lab03/symbol_logic.v
@@ -2,29 +2,26 @@ module symbol_logic(
     input s,            // Operation selector (0 = add, 1 = subtract)
     input carryOut,     // Carry out from the addition/subtraction operation
 
-    output [7:0] HEX4,  // display for symbol (+ or -)
-    output [7:0] HEX2,  // display for EQUAL SIGN (=)
-    output [7:0] HEX1   // display for carry out (1 or - or BLANK(hFF))    
+    output [7:0] op,    // display for symbol (+ or -)
+    output [7:0] eq,    // display for EQUAL SIGN (=)
+    output [7:0] prefix // display for carry out (1 or - or BLANK(hFF))    
 );
 
     // Internal logic for symbol displays
 
     // '=' symbol (segments to display '=')
-    assign HEX2 = 8'hB7; 
+    assign eq = 8'hB7; 
 
     // s = 1 '-' symbol: 8'hBF (segments to display '-')
     // s = 0 '+' symbol: 8'h8F (segments to display '+')    
-    assign HEX4 = s ? 8'hBF : 8'h8F;
+    assign op = s ? 8'hBF : 8'h8F;
 
-    // Internal logic for HEX1 display
+    // Internal logic for prefix display
     // '-' symbol: 8'hBF (when s = 1 and carryOut = 1)
     // '1' symbol: 8'hF9 (when s = 0 and carryOut = 1)
     // Blank:      8'hFF (otherwise)
-    assign HEX1 = (s && carryOut) ? 8'hBF   :   // '-'
+    assign prefix = (s && carryOut) ? 8'hBF :   // '-'
                   (!s && carryOut) ? 8'hF9  :   // '1'
                   8'hFF;                        // Blank
-
-
-    
 
 endmodule
