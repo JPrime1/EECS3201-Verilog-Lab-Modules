a8dfad119ff9915e68fa056b405ce79649ca97a4
JPrime1 <James.Prime.Dev@gmail.com>
2026-02-27 17:22:41 -0500

    finished layout for lab03 top

--- Lab03/Lab03.v
+++ Lab03/Lab03.v
@@ -2,33 +2,51 @@ module Lab03 (
     input  [9:0] SW,
     output [7:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5
 );
-
-    wire [3:0] a0;
-    wire [3:0] a1;
-    wire s;
-
+    // Extracting inputs from switches
+    wire [3:0] a0;  // 4-bit input A0
+    wire [3:0] a1;  // 4-bit input A1
+    wire s;         // Operation selector (0 for addition, 1 for subtraction)
     assign a0 = SW[7:4];
     assign a1 = SW[3:0];
     assign s  = SW[9];
 
-    wire [3:0] F;
-    wire Cout;
-
-    display_logic disp (
-        //DISPLAY LAYOUT
-        //HEX5 A0
+    // ALU operation
+    wire [3:0] F;   // 4-bit result from ALU
+    wire Cout;      // Carry out from ALU
+    add_sub alu (
         .a0(a0),
-        .HEX5(HEX5),
-
-        //HEX4 +/-
-        
-        //HEX3 A1        
         .a1(a1),
-        .HEX3(HEX3)
+        .s(s),
+        .result(F),
+        .carryOut(Cout)
+    );
 
-        //HEX2 =        
-        //HEX1 1/-
-        //HEX0 F (result)                
+    // Symbol Logic for Display
+    wire [7:0] op;     
+    wire [7:0] eq;     
+    wire [7:0] prefix;
+    symbol_logic sym (
+        .s(s),
+        .carryOut(Cout),
+        .op(op),
+        .eq(eq),
+        .prefix(prefix)
+    );
+
+    // Display Logic for HEX displays
+    display_logic disp (
+        .a0(a0),         
+        .a1(a1),
+        .op(op),
+        .eq(eq),
+        .prefix(prefix),
+        .result(F),
+        .HEX0(HEX0),
+        .HEX1(HEX1),
+        .HEX2(HEX2),
+        .HEX3(HEX3),
+        .HEX4(HEX4),
+        .HEX5(HEX5)
     );
 
 endmodule
