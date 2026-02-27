d58ae28f6870cfa9b5708a005b9726f3005fd3d4
JPrime1 <James.Prime.Dev@gmail.com>
2026-02-27 17:09:44 -0500

    display logic complete, wiring has been setup

--- Lab03/display_logic.v
+++ Lab03/display_logic.v
@@ -9,27 +9,23 @@ module display_logic(
 
     output [7:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5
 );
-    //setup wiring
-    wire [3:0] a0_disp;
-    wire [3:0] a1_disp;
-
-    //assign input to wires
-    assign a0_disp = a0;
-    assign a1_disp = a1;
-
     //DISPLAY LAYOUT
     //HEX5 A0
-    hex_display_decoder d0 (.in(a0_disp), .hex_out(HEX5));
+    hex_display_decoder d5 (.in(a0), .hex_out(HEX5));
 
     //HEX4 +/-
+    hex_display_decoder d4 (.in(op), .hex_out(HEX4));
     
     //HEX3 A1
-    hex_display_decoder d2 (.in(a1_disp), .hex_out(HEX3));
+    hex_display_decoder d3 (.in(a1), .hex_out(HEX3));
     
     //HEX2 =
+    hex_display_decoder d2 (.in(eq), .hex_out(HEX2));
+    
     //HEX1 1/-
+    hex_display_decoder d1 (.in(prefix), .hex_out(HEX1));
+    
     //HEX0 F (result)
-    
-    
+    hex_display_decoder d0 (.in(result), .hex_out(HEX0));          
 
 endmodule
