7644949224f0134d16290d9282ca47d9ecfc1d1c
JPrime1 <James.Prime.dev@gmail.com>
2026-02-27 22:28:50 -0500

    corrected display input

--- Lab03/display_logic.v
+++ Lab03/display_logic.v
@@ -10,22 +10,22 @@ module display_logic(
     output [7:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5
 );
     //DISPLAY LAYOUT
-    //HEX5 A0
+    // HEX5 A0
     hex_display_decoder d5 (.in(a0), .hex_out(HEX5));
 
-    //HEX4 +/-
-    hex_display_decoder d4 (.in(op), .hex_out(HEX4));
+    // connect op to HEX4 for '+' or '-'
+    assign HEX4 = op; 
     
-    //HEX3 A1
+    // HEX3 A1
     hex_display_decoder d3 (.in(a1), .hex_out(HEX3));
     
-    //HEX2 =
-    hex_display_decoder d2 (.in(eq), .hex_out(HEX2));
+    // connect eq to HEX2 for '='
+    assign HEX2 = eq;
     
-    //HEX1 1/-
-    hex_display_decoder d1 (.in(prefix), .hex_out(HEX1));
+    // connect prefix to HEX1 for carry out display (1, - or BLANK)
+    assign HEX1 = prefix;
     
-    //HEX0 F (result)
+    // HEX0 F (result)
     hex_display_decoder d0 (.in(result), .hex_out(HEX0));          
 
 endmodule
