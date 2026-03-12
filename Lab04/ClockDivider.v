4452057b2fb9aaf4dfa10a905839f841776cf408
JPrime1 <James.Prime.dev@gmail.com>
2026-03-12 14:50:06 -0400

    finalized clock

--- Lab04/ClockDivider.v
+++ Lab04/ClockDivider.v
@@ -1,6 +1,24 @@
-module ClockDivider (
+// Converts a 50 MHz clock to a 1 Hz clock for the shot clock counter
+
+module ClockDivider(
     input  cin,         // Clock input
     output reg cout     // Clock output (divided)
 );
+
+    reg [24:0] count;               // 25-bit counter to count 25 Million clock cycles
+    parameter half = 32'd25_000_000;// Half of 50 million for 1 Hz output
+
+    // On every rising edge of the input clock, increment the counter
+    always @(posedge cin) begin
+
+        if (count >= half - 1) begin
+            count <= 0;     // Reset counter
+            cout <= ~cout;  // Toggle output clock
+        end 
+        
+        else begin
+            count <= count + 1; // Increment counter
+        end
+    end
 
 endmodule
