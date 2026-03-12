d8630b7a04807c59b609c246fbbbd1c17e046dfc
JPrime1 <James.Prime.dev@gmail.com>
2026-03-12 14:57:36 -0400

    init simple maths for BCD

--- Lab04/BinaryToBCD.v
+++ Lab04/BinaryToBCD.v
@@ -1,7 +1,13 @@
+// Convert 5-bit binary input to two BCD digits (tens and ones)
+// Implement simple arithmetic: tens = binary_in / 10, ones = binary_in % 10
+
 module BinaryToBCD(
-    input [4:0] binary_in,      // 5-bit binary input
-    output reg [3:0] bcd_tens,  // BCD tens digit
-    output reg [3:0] bcd_ones   // BCD ones digit
+    input [4:0] binary_in,  // 5-bit binary input
+    output [3:0] bcd_tens,  // BCD tens digit
+    output [3:0] bcd_ones   // BCD ones digit
 );
     
+    assign bcd_tens = binary_in / 10; // Integer division to get tens digit
+    assign bcd_ones = binary_in % 10; // Modulo operation to get ones digit
+
 endmodule
